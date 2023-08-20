e5303535568c79e264f0939a35eadf1d4b8c40ec
chosohi <chosohi@naver.com>
2023-08-20 20:13:22 +0900

    baekjoon: N과 M(1)

--- baekjoon/재귀/N과 M (1)/solution.py
+++ baekjoon/재귀/N과 M (1)/solution.py
@@ -1,16 +1,32 @@
 import sys
-sys.stdin = open('baekjoon/재귀/N과 M (1)/input.txt', 'r') #file open
+sys.stdin = open('baekjoon/재귀/N과 M (1)/input.txt', 'r')
 
-N = int(input())
+# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열 
+
+T = int(input())
+
+for tc in range(T):
+    print("tc",tc)
+    N, M = list(map(int,input().split()))
 
 
-res = 0
+    arr = []
+    
 
-for i in range(N) :
-    tmp = i
-    for j in range(len(str(N))):
-        tmp += int(j)
-    if tmp == N and tmp < res:
-        res = tmp
+    def recur(cur):
+        if cur == M:
+            
+            print(" ".join(map(str, arr)))
+            return
+        
+        
+        for i in range(1, N+1):
+            if i not in arr:
+                arr.append(i)
+                recur(cur+1)
+                arr.pop()
+            
+            
 
-print(res)
+    recur(0)
+    
