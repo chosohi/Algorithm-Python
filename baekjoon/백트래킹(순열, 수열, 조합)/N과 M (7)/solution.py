509125e077917d82405d23cf1c191e8ad5fad1f6
chosohi <chosohi@naver.com>
2023-08-27 13:02:51 +0900

    baekjoon: N과 M(7)

--- baekjoon/백트래킹(순열, 수열, 조합)/N과 M (7)/solution.py
+++ baekjoon/백트래킹(순열, 수열, 조합)/N과 M (7)/solution.py
@@ -1,16 +1,26 @@
 import sys
-sys.stdin = open('baekjoon/백트래킹(순열, 수열, 조합)/N과 M (1)/input.txt', 'r') #file open
+sys.stdin = open('baekjoon/백트래킹(순열, 수열, 조합)/N과 M (7)/input.txt', 'r') #file open
 
-N = int(input())
+# N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
 
+# N개의 자연수 중에서 M개를 고른 수열
+# 같은 수를 여러 번 골라도 된다.
 
-res = 0
+T = int(input())
 
-for i in range(N) :
-    tmp = i
-    for j in range(len(str(N))):
-        tmp += int(j)
-    if tmp == N and tmp < res:
-        res = tmp
+for tc in range(T):
+    N, M = list(map(int, input().split()))
+    arr = sorted(list(map(int, input().split())))
+    res = []
 
-print(res)
+    def recur():
+        if len(res) == M :
+            print(" ".join(map(str, res)))
+            return
+        
+        for i in range(N):
+            res.append(arr[i])
+            recur()
+            res.pop()
+
+    recur()
