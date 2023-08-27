02e0445cd44959652d333e52498e2f8822e3d397
chosohi <chosohi@naver.com>
2023-08-27 12:11:47 +0900

    baekjoon: N과 M(4)

--- baekjoon/재귀/N과 M (4)/solution.py
+++ baekjoon/재귀/N과 M (4)/solution.py
@@ -1,16 +1,25 @@
 import sys
-sys.stdin = open('baekjoon/재귀/N과 M (1)/input.txt', 'r') #file open
+sys.stdin = open('baekjoon/재귀/N과 M (4)/input.txt', 'r') #file open
 
-N = int(input())
+# 1부터 N까지 자연수 중에서 M개를 고른 수열
+# 같은 수를 여러 번 골라도 된다.
+# 고른 수열은 비내림차순이어한다.
 
+T = int(input())
 
-res = 0
+for tc in range(T):
+    N, M = list(map(int, input().split()))
+    arr = []
 
-for i in range(N) :
-    tmp = i
-    for j in range(len(str(N))):
-        tmp += int(j)
-    if tmp == N and tmp < res:
-        res = tmp
-
-print(res)
+    def recur() :
+        if len(arr) == M:
+            print(" ".join(map(str, arr)))
+            return
+        for i in range(1,N+1) :
+            if len(arr) and i < arr[-1] :
+                continue
+            arr.append(i)
+            recur()
+            arr.pop()
+              
+    recur()
