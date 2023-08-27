8331225322131da58b3c3723b8bd0ef5bdf40906
chosohi <chosohi@naver.com>
2023-08-27 13:04:24 +0900

    baekjoon: N과 M(8)

--- baekjoon/백트래킹(순열, 수열, 조합)/N과 M (8)/solution.py
+++ baekjoon/백트래킹(순열, 수열, 조합)/N과 M (8)/solution.py
@@ -1,16 +1,30 @@
 import sys
-sys.stdin = open('baekjoon/백트래킹(순열, 수열, 조합)/N과 M (1)/input.txt', 'r') #file open
+sys.stdin = open('baekjoon/백트래킹(순열, 수열, 조합)/N과 M (8)/input.txt', 'r') #file open
 
-N = int(input())
+# N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다.
 
+# N개의 자연수 중에서 M개를 고른 수열
+# 같은 수를 여러 번 골라도 된다.
+# 고른 수열은 비내림차순이어야 한다.
+# 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
 
-res = 0
+T = int(input())
 
-for i in range(N) :
-    tmp = i
-    for j in range(len(str(N))):
-        tmp += int(j)
-    if tmp == N and tmp < res:
-        res = tmp
+for tc in range(T):
+    N, M = list(map(int, input().split()))
+    arr = sorted(list(map(int, input().split())))
+    res = []
 
-print(res)
+    def recur():
+        if len(res) == M :
+            print(" ".join(map(str, res)))
+            return
+        
+        for i in range(N):
+            if len(res) and res[-1] > arr[i]:
+                continue
+            res.append(arr[i])
+            recur()
+            res.pop()
+
+    recur()
