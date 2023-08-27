ba8fa705fa9d4d8d403cde66cc159821791cf310
chosohi <chosohi@naver.com>
2023-08-27 11:47:26 +0900

    baekjoon: N과 M(3)

--- baekjoon/재귀/N과 M (3)/solution.py
+++ baekjoon/재귀/N과 M (3)/solution.py
@@ -1,16 +1,24 @@
 import sys
 sys.stdin = open('baekjoon/재귀/N과 M (3)/input.txt', 'r') #file open
+ 
+# 1부터 N까지 자연수 중에서 M개를 고른 수열
+# 같은 수를 여러 번 골라도 된다.
 
-N = int(input())
+T = int(input())
 
+for tc in range(T) :
+    N, M = list(map(int, input().split()))
+    arr = []
 
-res = 0
-
-for i in range(N) :
-    tmp = i
-    for j in range(len(str(N))):
-        tmp += int(j)
-    if tmp == N and tmp < res:
-        res = tmp
-
-print(res)
+    def recur() :
+        if len(arr) == M:
+            print(" ".join(map(str, arr)))
+            return
+        
+        for i in range(1, N+1):
+            arr.append(i)
+            recur()
+            arr.pop()
+        
+        
+    recur()
